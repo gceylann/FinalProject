6860bcae108a41019193fbb2bed01843ecfb96ab
gceylan <gulsumceylan1998@gmail.com>
2021-03-03 20:17:14 +0300

    14. gün son

--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -7,9 +7,9 @@ namespace Core.Utilities.Security.Encryption
 {
     public class SigningCredentialsHelper
     {
-    public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
         {
             return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
         }
-     
+    }
 }
--- Core/Utilities/Security/JWT/ITokenHelper.cs
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -1,5 +1,4 @@
 ﻿using Core.Entities.Concreate;
-using System;
 using System.Collections.Generic;
 using System.Text;
 
